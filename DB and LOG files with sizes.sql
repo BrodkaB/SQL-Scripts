1e3960e79b186d1cab027530c6bcc3dc1f4fcf52
BrodkaB <97090245+BrodkaB@users.noreply.github.com>
2022-01-05 13:38:53 +0100

    Update DB and LOG files with sizes.sql

--- DB and LOG files with sizes.sql
+++ DB and LOG files with sizes.sql
@@ -1,8 +1,9 @@
-
 -----------------DB files
-SELECT name, CAST(ROUND(size*8*1024/1024/1024,1) AS numeric (10,2)) FROM sys.master_files
+SELECT name, CAST(ROUND(size*8*1024/1024/1024,1) AS numeric (10,2)) 
+FROM sys.master_files
 WHERE type_desc= 'ROWS'
 
 --------------------LOG files
-SELECT name, CAST(ROUND(size*8*1024/1024/1024,1) AS numeric (10,2)) FROM sys.master_files
+SELECT name, CAST(ROUND(size*8*1024/1024/1024,1) AS numeric (10,2)) 
+FROM sys.master_files
 WHERE type_desc= 'LOG'
