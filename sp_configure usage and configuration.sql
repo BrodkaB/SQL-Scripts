23c51bc2444591e4fac3239a32e61527fa5752e3
BrodkaB <97090245+BrodkaB@users.noreply.github.com>
2022-01-21 14:03:40 +0100

    Update and rename Enabling xp_cmdshell.sql to sp_configure usage and configuration.sql

--- sp_configure usage and configuration.sql
+++ sp_configure usage and configuration.sql
@@ -1,3 +1,7 @@
+---sp_configuration is an imporatnt stored procedure which allows to change SQL Server setting via T-SQL, changing stuff like default backup compression, memory usage---
+----Below example how to change default sp_configure setting to advanced one and enable xp_cmdshell command----
+
+
 ---Enabling sp_configure advanced options
 EXEC sp_configure 'show advanced options', '1'
 RECONFIGURE
