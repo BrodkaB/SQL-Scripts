ef1a6dac1ca2c6791499a5a5bb5a22d7ab56806d
BrodkaB <97090245+BrodkaB@users.noreply.github.com>
2022-04-15 13:18:19 +0200

    Create Powershell SPN.ps1

---
+++ Powershell SPN.ps1
@@ -0,0 +1,23 @@
+----Listowanie wszystkich SPN----
+
+setspn -L
+
+---Listowanie SPN dla danego użytkownika---
+
+setspn -L nazwa_domeny\nazwa_konta
+
+---Listowanie SPN dla danego servera (network server name i OS cluster name w przypadku klastra)---
+
+setspn -L nazwa
+
+---Dodawanie SPN dla użytkownika (SQL service account)---
+
+setspn -A MSSQLSvc/nazwa_klastra nazwa_domeny\nazwa_użytkownika
+setspn -A MSSQLSvc/nazwa_klastra.cho.corp.ryanair.com nazwa_domeny\nazwa_uzytkownika
+
+---Dodawanie SPN dla konkretnej instancji---
+
+setspn -A MSSQLSvc/nazwa_klastra:nazwa_instancji nazwa_domeny\nazwa_użytkownika
+setspn -A MSSQLSvc/nazwa_klastra.cho.corp.ryanair.com:nazwa_instancji nazwa_domeny\nazwa_uzytkownika
+
+---W celu usunięcia danego SPN (w przypadku pojawienia się duplikatu), parametr -A zamieniamy na parametr -D---
