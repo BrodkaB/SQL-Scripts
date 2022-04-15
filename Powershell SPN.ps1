d98f525b05a879c31780b10a74ef21d4d86d563c
BrodkaB <97090245+BrodkaB@users.noreply.github.com>
2022-04-15 13:19:23 +0200

    Update Powershell SPN.ps1

--- Powershell SPN.ps1
+++ Powershell SPN.ps1
@@ -1,23 +1,23 @@
-----Listowanie wszystkich SPN----
+#Listowanie wszystkich SPN
 
 setspn -L
 
----Listowanie SPN dla danego użytkownika---
+#Listowanie SPN dla danego użytkownika
 
 setspn -L nazwa_domeny\nazwa_konta
 
----Listowanie SPN dla danego servera (network server name i OS cluster name w przypadku klastra)---
+#Listowanie SPN dla danego servera (network server name i OS cluster name w przypadku klastra)
 
 setspn -L nazwa
 
----Dodawanie SPN dla użytkownika (SQL service account)---
+#Dodawanie SPN dla użytkownika (SQL service account)
 
 setspn -A MSSQLSvc/nazwa_klastra nazwa_domeny\nazwa_użytkownika
 setspn -A MSSQLSvc/nazwa_klastra.cho.corp.ryanair.com nazwa_domeny\nazwa_uzytkownika
 
----Dodawanie SPN dla konkretnej instancji---
+#Dodawanie SPN dla konkretnej instancji
 
 setspn -A MSSQLSvc/nazwa_klastra:nazwa_instancji nazwa_domeny\nazwa_użytkownika
 setspn -A MSSQLSvc/nazwa_klastra.cho.corp.ryanair.com:nazwa_instancji nazwa_domeny\nazwa_uzytkownika
 
----W celu usunięcia danego SPN (w przypadku pojawienia się duplikatu), parametr -A zamieniamy na parametr -D---
+#W celu usunięcia danego SPN (w przypadku pojawienia się duplikatu), parametr -A zamieniamy na parametr -D
