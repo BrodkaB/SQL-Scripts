be33444219009e5c1f657f1b08f257b1452f5a8e
BrodkaB <97090245+BrodkaB@users.noreply.github.com>
2024-03-11 14:26:45 +0100

    Update DB Snapshot.sql

--- DB Snapshot.sql
+++ DB Snapshot.sql
@@ -13,7 +13,7 @@ RESTORE DATABASE Databasename FROM DATABASE_SNAPSHOT = 'ss_Databasename_ss'
 ---creating database snapshot with multiple files---
 CREATE DATABASE db_snapshot_ss
 ON
-(NAME = [Frimary database file name],
+(NAME = [Primary database file name],
  FILENAME ='location of snapshot file\db_snapshot_ss.SS1'),
 (NAME = [Secondary database file name],
  FILENAME ='location of snapshot file\db_snapshot_ss.SS2')
